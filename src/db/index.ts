67a2532c3dc89d5ad8c7f1257fa8e51551e2e144
tyree <tyree.fullstack@gmail.com>
2026-05-22 14:51:29 -0400

    fix: adding logging but not throwing error in build

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { logger } from "@/lib/logger";
 
-if (!process.env.DATABASE_URL) throw Error("No database url found in env");
+if (!process.env.DATABASE_URL) logger.fatal("database env variable not found");
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL ?? "");
